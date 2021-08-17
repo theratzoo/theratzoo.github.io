e44733b091c4785e34652ea2bf252cfe6347444c
Luke <58711330+theratzoo@users.noreply.github.com>
2021-08-17 13:55:28 -0400

    Update footer.php

--- dntcentral/scripts/footer.php
+++ dntcentral/scripts/footer.php
@@ -3,7 +3,7 @@
 	<br>
 	<h5>Newsletter</h5>
     <h6>Keep up with the latest articles, Mulligan Games, matchup guides, and other D&T information.</h6>
-    <form method="post" action="/newsletter.php">
+    <form method="post" action="/dntcentral/newsletter.php">
         <input type="text" id="emailBar" placeholder="enter email" name="mail" required onblur="validateEmail(this);">
         <input type="submit" id="emailBtn" value="Subscribe">
         <textarea name="pagename" id="email-p" value="" style="display:none"></textarea>
@@ -12,7 +12,7 @@
 	<h6 class="footH">Questions or suggestions? Email me at <a href="mailto:theratzoo@dntcentral.com">theratzoo@dntcentral.com</a></h6>
 	
 	<h6 class="footH"><a href="https://mobile.twitter.com/TaxesCentral">Twitter</a></h6>
-	<h6 class="footH"><a href="/privacy-policy">Privacy Policy</a></h6>
+	<h6 class="footH"><a href="/dntcentral/privacy-policy">Privacy Policy</a></h6>
 	
 	<h6 class="footH">Dntcentral is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.</h6>
 	<br>
