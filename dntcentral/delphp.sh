fe8b6aa046c003fed6f93636f0020e2c2436e5dd
theratzoo <lderatzou@wpi.edu>
2021-08-21 14:34:27 -0400

    big changes

--- dntcentral/delphp.sh
+++
@@ -1,3 +0,0 @@
-for i in $(find . -name '*.html'); do
-	sed -e "s/style\.css/dntcentral\/style.css/g" -i $i
-done
--- dntcentral/navbarify.sh
+++
@@ -1,7 +0,0 @@
-# we need to add after  <body class="homePage" onload="loadScript()"> 
-# MAKE SURE THAT NAVBAR STARTS WITH BODY!
-navbar = `cat navbar.php`
-line = '<body class="homePage" onload="loadScript()">'
-for i in $(find . -name '*.html'); do
-	sed -i "s/$line/$line/g" $i
-done
